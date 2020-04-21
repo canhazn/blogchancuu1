531ded037f3667a31673f4bd1539388f55d4b973
tuan anh <canhazn@gmail.com>
2020-04-21 22:31:49 +0700

    c

--- blogchancuu/wsgi.py
+++ blogchancuu/wsgi.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise.django import DjangoWhiteNoise
+
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my-application.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogchancuu.settings')
+
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
